53102f2a0190db3e8996f31bf9312a4ff314266d
bgrana <bgrana@conwet.com>
2016-03-30 14:45:32 +0200

    Implement main properties validator

--- src/validator/mainPropertiesvalidator.js
+++ src/validator/mainPropertiesvalidator.js
@@ -1,5 +1,52 @@
 'use strict';
 
 module.exports.validateMainProperties = function(mainProperties) {
-
+    if (
+        validateName(mainProperties.name) &&
+        validateVendor(mainProperties.vendor) &&
+        validateVersion(mainProperties.version) &&
+        validateSize(mainProperties.size) &&
+        validateNamespace(mainProperties.xmlns)
+    ) {
+        return true;
+    }
+    return false;
 };
+
+function validateName(name) {
+    if (!name || name.length === 0) {
+        return false;
+    }
+    return true;
+}
+
+function validateVendor(vendor) {
+    if (!vendor || vendor.length === 0) {
+        return false;
+    }
+    return true;
+}
+
+function validateVersion(version) {
+    if (!version || version.length === 0) {
+        return false;
+    }
+    var re = /^([1-9]\d*\.|0\.)*([1-9]\d*|0)((a|b|rc)[1-9]\d*)?$/;
+    return re.test(version);
+}
+
+function validateSize(size) {
+    if (!size) {
+        return true;
+    }
+    var re = /^\d+(\.\d+)?\s*(px|%)?$/;
+    return re.test(size);
+}
+
+function validateNamespace(ns) {
+    if (!ns || ns.length === 0) {
+        return false;
+    }
+    var re = /^\c+:.+$/;
+    re.test(ns);
+}
