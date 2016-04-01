5e5d0fe7724ce56323ae9183e21717e55c08e2e8
bgrana <bgrana@conwet.com>
2016-04-01 14:17:06 +0200

    Use libxmljs validator intead of making my own

--- src/ConfigParser.js
+++ src/ConfigParser.js
@@ -1,22 +1,41 @@
 'use strict';
 
 var fs = require('fs');
+var libxml = require('libxmljs');
+var request = require('request');
 var parseString = require('xml2js').parseString;
-var validator = require('./validator/validator');
+var Promise = require('es6-promise');
 
-function ConfigParser() {};
+function ConfigParser() {
+    this.data = null;
+    this.componentData = null;
+};
 
 ConfigParser.prototype = {
-    setFile: function (configFile) {
-        fs.readFile(configFile, function (err, data) {
-            if (err) {throw err;}
-            parseString(data, function (err, data) {
-                if (err) {throw err;}
-                if (!validator.validate(data)) {
-                    throw 'Invalid config.xml structure';
-                }
-                this.componentData = data[validator.getType(data)];
-                this.data = data;
+    setFile: function (configFile, callback) {
+        return new Promise(function (resolve, reject) {
+            // Read File
+            fs.readFile(configFile, function (err, data) {
+                if (err) {reject(err);}
+                var xml = libxml.parseXml(data);
+
+                // Get XSD schema
+                request.get('https://raw.githubusercontent.com/Wirecloud/wirecloud/develop/src/wirecloud/commons/utils/template/schemas/xml_schema.xsd', function (err, response) {
+                    if (err) {reject(err);}
+                    var schema = libxmljs.parseXml(response.body);
+                    var valid = validate(schema, xml);
+                    if (!valid) {
+                        reject(xml.validationErrors);
+                    }
+
+                    // Parse XML to JS object
+                    parseString(data, function (err, data) {
+                        if (err) {reject(err);}
+                        this.data = data;
+                        this.componentData = data[getType(data)];
+                        resolve();
+                    }
+                });
             });
         });
     },
@@ -33,5 +52,25 @@ ConfigParser.prototype = {
         return this.componentData.$.version;
     }
 };
+
+function validate(schema, data) {
+    return data.validate(schema);
+}
+
+function getType (data) {
+    switch (data) {
+        case data.widget:
+            return 'widget';
+            break;
+        case data.operator:
+            return 'operator';
+            break;
+        case data.mashup:
+            return 'mashup';
+            break;
+        default:
+            return 'unknown';
+    }
+}
 
 module.exports = ConfigParser;
