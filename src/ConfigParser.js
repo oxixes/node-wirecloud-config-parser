f2c3178b46d9607902e76a2e1d8f740d8c97305b
bgrana <bgrana@conwet.com>
2016-04-05 14:42:24 +0200

    Refactor setFile function

--- src/ConfigParser.js
+++ src/ConfigParser.js
@@ -3,24 +3,60 @@
 var fs = require('fs');
 var libxml = require('libxmljs');
 var request = require('request');
-var parseString = require('xml2js').parseString;
+var xml2js = require('xml2js');
 var Promise = require('es6-promise').Promise;
+var SCHEMA_URL = 'https://raw.githubusercontent.com/Wirecloud/wirecloud/develop/src/wirecloud/commons/utils/template/schemas/xml_schema.xsd';
 
 function validate(schema, data) {
     return data.validate(schema);
 }
 
 function getType (data) {
-    switch (data) {
-        case data.widget:
-            return 'widget';
-        case data.operator:
-            return 'operator';
-        case data.mashup:
-            return 'mashup';
-        default:
-            return 'unknown';
+    var result;
+    if(data.widget) {
+        result = 'widget';
+    } else if (data.operator) {
+        result = 'operator';
+    } else {
+        result = 'mashup';
     }
+}
+
+function validateXml(xml) {
+    return new Promise(function (resolve, reject) {
+        request.get(SCHEMA_URL, function (err, response, body) {
+            if (err) {
+                reject(err);
+                throw err;
+            }
+            var schema;
+            try {
+                schema = libxml.parseXml(body);
+            } catch (e) {
+                reject(e);
+                throw e;
+            }
+            if (!validate(schema, xml)) {
+                reject('Invalid config file');
+                throw xml.validationErrors;
+            }
+            resolve();
+        });
+    });
+}
+
+function parse2js(configData) {
+    return new Promise(function (resolve, reject) {
+        xml2js.parseString(configData, function (err, data) {
+            if (err) {
+                reject(err);
+                throw err;
+            }
+            this.data = data;
+            this.componentData = data[getType(data)];
+            resolve();
+        }.bind(this));
+    }.bind(this));
 }
 
 function ConfigParser() {
@@ -31,29 +67,17 @@ function ConfigParser() {
 ConfigParser.prototype = {
     setFile: function (configFile) {
         return new Promise(function (resolve, reject) {
-            // Read File
-            fs.readFile(configFile, function (err, data) {
-                if (err) {throw err;}
-                var xml = libxml.parseXml(data);
-
-                // Get XSD schema
-                request.get('https://raw.githubusercontent.com/Wirecloud/wirecloud/develop/src/wirecloud/commons/utils/template/schemas/xml_schema.xsd', function (err, response, body) {
-                    if (err) {reject(err);}
-                    var schema = libxml.parseXml(body);
-                    var valid = validate(schema, xml);
-                    if (!valid) {
-                        reject(xml.validationErrors);
-                    }
-
-                    // Parse XML to JS object
-                    parseString(data, function (err, data) {
-                        if (err) {reject(err);}
-                        this.data = data;
-                        this.componentData = data[getType(data)];
-                        resolve("ok");
-                    }.bind(this));
-                }.bind(this));
-            }.bind(this));
+            var configData, xml;
+            try {
+                configData = fs.readFileSync(configFile).toString();
+                xml = libxml.parseXml(configData);
+            } catch (e) {
+                reject(e);
+                throw e;
+            }
+            validateXml(xml)
+                .then(parse2js.bind(this, configData))
+                .catch(reject);
         }.bind(this));
     },
     getData: function () {
